e6cc96fa6ce4a42c1e59a75b7c53ebfbaf642d6e
Christian Silfang <Christian Silfang>
2017-06-15 00:52:23 +0200

    added body coponent, added first header component

---
+++ src/fw/container/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fw-container-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class FwContainerHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
